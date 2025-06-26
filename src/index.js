1980c570c112fd9ba13fcef05c6f6d695ae666ca
Tejas544 <tejas2332rajput@gmail.com>
2025-06-26 22:43:28 +0530

    connct to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path : './env'
+})
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express';
+
+const app = express();
+
+( async ()=>{
+   try {
+     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error" , (error) => {
+        console.log("Error: " , error)
+    })
+    app.listen(process.env.PORT , () => {
+        console.log(`server listening on port ${process.env.PORT}`);
+    })
+
+   } catch (error) {
+    console.error("ERR" , error);
+    throw error
+   }
+})()
+   */
